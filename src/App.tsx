48fd36057780877494da0e3a49f66b3c3bcce1ae
Alim Khan <90334104+Mh-Alim@users.noreply.github.com>
2025-03-07 21:55:12 +0530

    tailwind css added

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <button className='bg-red-500'>Clikc me</button>
+    </>
+  )
+}
+
+export default App
